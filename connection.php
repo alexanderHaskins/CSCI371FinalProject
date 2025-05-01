0c473ae5d901a587e54f40d142628cd186bccc03
JMartin28 <jamie.l.martin@ndus.edu>
2025-05-01 08:33:40 -0500

    Update connection.php
    connection to database on instructor side

--- connection.php
+++ connection.php
@@ -1,22 +1,16 @@
 <?php
 /* Database credentials */
-$servername = "rei.cs.ndsu.nodak.edu";
-$username = "jamie_l_martin_371s25";
-$password = "cpONE313ON0!";
-$database = "jamie_l_martin_db371s25";
-
 define('DB_SERVER', 'rei.cs.ndsu.nodak.edu');
 define('DB_USERNAME', 'jamie_l_martin_371s25');
 define('DB_PASSWORD', 'cpONE313ON0!');
 define('DB_NAME', 'jamie_l_martin_db371s25');
 
 /* Connect to MySQL database */
-$conn = mysqli_connect($servername, $username, $password, $database);
+$conn = mysqli_connect(DB_SERVER, DB_USERNAme, DB_PASSWORD, DB_NAME);
 
 // Check connection
-if ($conn -> false) {
-    die("Connection failed: " . $connection -> connect_error);
-} 
-
+if ($conn === false) {
+    die("Connection failed: " . mysqli_connect_error());
+}
 echo "Connected successfully";
 ?>
