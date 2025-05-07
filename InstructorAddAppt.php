c409d352516604ca7c7569a27dfad232ae4922cd
JMartin28 <jamie.l.martin@ndus.edu>
2025-05-07 13:41:21 -0500

    Add files via upload
    Adding appointments by instructor

---
+++ InstructorAddAppt.php
@@ -0,0 +1,26 @@
+<?php
+// This file shows the instructor adding an appointment
+
+session_start();
+
+include"header.html";
+include"footer.html";
+include'connection.php';
+
+    $date = $_POST['appointmentDate'];
+    $time = $_POST['appointmentTime'];
+    $group = $_POST['appointmentGroupID'];
+    $team = $_POST['appointmentTeamName'];
+    $date = $_POST["userId"];
+    $date = $_POST["appointmentID"];
+
+
+//creating a new appointment 
+    $sql = "UPDATE appointments SET appointmentDate=" . $date . ", appointmentTime=" . $time .
+    ", appointmentGroupID=" . $group . ", appointmentTeamName=" . $email . ", userID=" . $user .
+    "WHERE appointmentID=".$appointment;
+
+header("location: InstructorView.php");
+$conn -> close();
+?>
+
