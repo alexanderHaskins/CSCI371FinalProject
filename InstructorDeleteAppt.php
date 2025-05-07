98ab28ba92fcfebe576f15f2e45e91e2a6b3f0c3
JMartin28 <jamie.l.martin@ndus.edu>
2025-05-07 08:42:29 -0500

    InstructorDeleteAppt.php
    Changed file name and formatted the code lines.

---
+++ InstructorDeleteAppt.php
@@ -0,0 +1,26 @@
+<?php
+// This file showws the action of deleting appointments by the Instructor
+
+// Start the session
+session_start();
+include("headerInstructor.html");
+include("footer.html");
+
+$login = $_SESSION['InstructorName'];
+echo $_SESSION['InstructorName'];
+
+//Connection to MySQL
+include("connection.php");
+$id = $_GET['appointmentID'];
+
+$sql = "DELETE FROM appointments WHERE id='' " . $_GET[$id];
+$result = $conn->query($sql);
+    if($conn->query($SQL) === TRUE){
+        echo "Record was deleted.";
+    }
+    else{
+        echo "Unable to execute $sql. " . mysqli_error($link);
+    }
+$conn->close();
+header("location: InstructorView.php");
+?>
