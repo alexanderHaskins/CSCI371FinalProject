1c35112d34670bab0f8849a42445a688009437fc
JMartin28 <jamie.l.martin@ndus.edu>
2025-05-07 09:52:56 -0500

    Add files via upload

---
+++ InstructorView.php
@@ -0,0 +1,48 @@
+
+
+
+<?php
+//File shown is the actions the instrcuctor can take with editing appointments
+session_start();
+
+include("header.html");
+include("footer.html");
+include'connection.php';
+
+$login = $_SESSION['InstructorName'];
+echo $_SESSION['InstructorName'];
+
+echo "<table border='1'>";
+"<tr>";
+"<th>Date</th>";
+"<th>Time</th>";
+"<th>Group Number</th>";
+"<th>Team Name</th>";
+"<th>Email</th>";
+"<th>Name</th>";
+"<th>Available</th>";
+"</tr>";
+
+
+if (mysql_num_rows($results) > 0) {
+    while ($results = mysql_fetch_array($results)) {
+        echo "<tr><td>" . $results['appointmentID'] .   "</td>";
+        echo "<td>" . $results['appointmentDate']   .   "</td>";
+        echo "<td>" . $results['appointmentTime'] .     "</td>";
+        echo "<td>" . $results['appointmentGroupID'] .  "</td>";
+        echo "<td>" . $results['appointmentTeamName'] . "</td>";
+        echo "<td>" . $results['appointmentEmail'] .    "</td>";
+        echo "<td>" . $results['userID'] . "</td></tr>";
+    }
+}
+echo "</table>";
+
+
+    $msg .='<h3><a href ="InstructorCreate.php"><Create Appointment</a></h3>';
+    $msg .='<h3><a href ="InstructorDeleteAppt.php"><Delete Appointment</a></h3>';
+    $msg .='<h3><a href ="InstructorUpdate"><Update Appointment</a></h3>';
+    $msg .='<h3><a href ="logout.php"><Logout</a></h3>';
+?>
+
+
+        
