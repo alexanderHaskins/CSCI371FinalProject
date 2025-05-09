35e95f2c579f15f5c99c027e0ee501e289999ef1
JMartin28 <jamie.l.martin@ndus.edu>
2025-05-09 13:33:37 -0500

    InstDeleteAppt.php

--- InstDeleteAppt.php
+++ InstDeleteAppt.php
@@ -1,23 +1,22 @@
 <?php
-// This file showws the action of deleting appointments by the Instructor
-
+// This file shows deleting appointments by the Instructor
 // Start the session
 session_start();
-include("header.html");
+include("headerInst.html");
 include("footer.html");
-include'connect.php;
+include'testConn.php';
 
 $login = $_SESSION['username'];
 echo $_SESSION['username'];
 
 //Connection to MySQL
-include("connection.php");
+include("testConn.php");
 $id = $_GET['appointmentID'];
 
 $sql = "DELETE FROM appointments WHERE id='' " . $_GET[$id];
 $result = $conn->query($sql);
     if($conn->query($SQL) === TRUE){
-        echo "Record was deleted.";
+        echo "appointment was deleted.";
     }
     else{
         echo "Unable to execute $sql. " . mysqli_error($link);
