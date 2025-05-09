67b19adf7187a6fede073b492b37852360d7c3a6
JMartin28 <jamie.l.martin@ndus.edu>
2025-05-09 10:26:29 -0500

    connect.php

--- connection.php
+++ connection.php
@@ -12,5 +12,4 @@ $conn = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
 if ($conn === false) {
     die("Connection failed: " . mysqli_connect_error());
 }
-echo "Connected successfully";
 ?>
